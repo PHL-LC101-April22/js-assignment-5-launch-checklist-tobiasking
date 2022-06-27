6e87fda319cbeaa018a090ea8be8c64be74c07d2
toby king <tobyjacquesking@gmail.com>
2022-06-27 13:29:22 -0400

    trying to get tasks to pass

--- script.js
+++ script.js
@@ -22,15 +22,15 @@ let form = document.querySelector ("form");
             formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
         })
 
-    let planetList;
+    let listedPlanets;
     let listedPlanetsResponse = myFetch();
     listedPlanetsResponse.then(function (result) {
-        planetList = result;
-        console.log(planetList);
+        listedPlanets = result;
+        console.log(listedPlanets);
     }).then(function (){
     // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-        console.log(planetList);
-        let planet = pickPlanet(planetList);
+        console.log(listedPlanets);
+        let planet = pickPlanet(listedPlanets);
         addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image);
     })
     
